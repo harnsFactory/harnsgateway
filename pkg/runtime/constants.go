ddf18ac89daf78a26b228a466b52b579fb94f572
MrYanchongbin <yanchokingbingo2524@gmail.com>
2023-10-08 00:25:27 +0800

    修改modbustcp起始寄存器地址为0

--- pkg/runtime/constants.go
+++ pkg/runtime/constants.go
@@ -56,7 +56,7 @@ var StringToDataType = map[string]DataType{
 	"bool":    BOOL,
 	"int16":   INT16,
 	"float32": FLOAT32,
-	"float64": FLOAT32,
+	"float64": FLOAT64,
 	"int32":   INT32,
 	"int64":   INT64,
 	"uint16":  UINT16,
@@ -74,4 +74,58 @@ var DataTypeWord = map[DataType]uint{
 	UINT16:  1,
 	NUMBER:  1,
 	STRING:  1,
+}
+
+type StopBits int
+
+const (
+	// OneStopBit sets 1 stop bit (default)
+	OneStopBit StopBits = iota
+	// OnePointFiveStopBits sets 1.5 stop bits
+	OnePointFiveStopBits
+	// TwoStopBits sets 2 stop bits
+	TwoStopBits
+)
+
+var StopBitsToString = map[StopBits]string{
+	OneStopBit:           "1",
+	OnePointFiveStopBits: "1.5",
+	TwoStopBits:          "2",
+}
+
+var StringToStopBits = map[string]StopBits{
+	"1":   OneStopBit,
+	"1.5": OnePointFiveStopBits,
+	"2":   TwoStopBits,
+}
+
+type Parity int
+
+const (
+	// NoParity disable parity control (default)
+	NoParity Parity = iota
+	// OddParity enable odd-parity check
+	OddParity
+	// EvenParity enable even-parity check
+	EvenParity
+	// MarkParity enable mark-parity (always 1) check
+	MarkParity
+	// SpaceParity enable space-parity (always 0) check
+	SpaceParity
+)
+
+var ParityToString = map[Parity]string{
+	NoParity:    "noParity",
+	OddParity:   "oddParity",
+	EvenParity:  "evenParity",
+	MarkParity:  "markParity",
+	SpaceParity: "spaceParity",
+}
+
+var StringToParity = map[string]Parity{
+	"noParity":    NoParity,
+	"oddParity":   OddParity,
+	"evenParity":  EvenParity,
+	"markParity":  MarkParity,
+	"spaceParity": SpaceParity,
 }
